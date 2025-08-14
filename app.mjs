f3ab954399af212f69b8efcd9ef5061c1738dd82
Art <artkt@hotmail.com>
2025-08-14 14:15:15 +0700

    feat: add deploy service

--- app.mjs
+++ app.mjs
@@ -1,39 +1,37 @@
 import express from "express";
 import bookRouter from "./routes/book.routes.mjs";
 import authRouter from "./routes/auth.routes.mjs";
-import dotenv from "dotenv";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
 async function init() {
-  dotenv.config();
   const app = express();
-  const port = 4002;
+  const port = process.env.PORT || 4002;
+
   const options = {
     definition: {
       openapi: "3.0.0",
       info: {
         title: "Book API",
         version: "1.0.0",
-        description: "RESTful API for managing users and books",
+        description: "RESTful API",
       },
-      servers: [
-        {
-          url: "http://localhost:4002",
-        },
+servers: [
+        { url: "https://your-render-url.onrender.com" }, // เปลี่ยนหลัง deploy
       ],
     },
     apis: ["./routes/*.mjs"],
   };
+
   const specs = swaggerJsdoc(options);
 
   app.use(express.json());
-  app.use('/books', bookRouter);
-  app.use('/auth', authRouter);
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+  app.use("/books", bookRouter);
+  app.use("/auth", authRouter);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
   app.listen(port, () => {
-    console.log(`Server is running at ${port}`);
+    console.log(`Server is running at port ${port}`);
   });
 }
 
